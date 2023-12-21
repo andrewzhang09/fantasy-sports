36a23bfd9636a7941212654ac6c998b4b621b9ee
Andrew Zhang <andrewzhang09@yahoo.com>
2023-12-20 21:24:49 -0800

    made back end directory

--- drews_fantasy_sports_helper/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from drews_fantasy_sports_helper.routes import main_bp
-
-app.register_blueprint(main_bp)
-
-import drews_fantasy_sports_helper.routes
